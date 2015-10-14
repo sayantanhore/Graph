d339602c67f6b7e3e202e94b2295fb1ca7606869
Sayantan Hore <sayantanhore@1BQQGE0.kumpula.dhcp.hupnet.helsinki.fi>
2015-10-14 19:06:35 +0300

    Initial commit for Graph

---
+++ static/js/generate_graph.js
@@ -0,0 +1,112 @@
+"use script";
+
+var CreateGraph = {
+    svg: null,
+    w: null,
+    h: null,
+    defs: null,
+    group: null,
+    grid: null,
+    createGraph: function(svg, w, h){
+        this.svg = svg;
+        this.w = w;
+        this.h = h;
+        this.createDefs();
+        this.createPatterns();
+        this.createGroup();
+        this.createGrid();
+        this.addAxis();
+        this.generateGraph();
+    },
+    createDefs: function(){
+        this.defs = this.svg.append("defs");
+    },
+    createPatterns: function(){
+        this.defs.append("pattern")
+                    .attr({
+                        "id": "small_block",
+                        "patternUnits": "userSpaceOnUse",
+                        "width": 10,
+                        "height": 10,
+                    })
+                    .append("path")
+                        .attr({
+                            "fill": "none",
+                            "stroke": "gray",
+                            "stroke-width": "1",
+                            "d": "M 10 0 L 0 0 0 10"
+                        })
+        var large_block = this.defs.append("pattern")
+                    .attr({
+                        "id": "large_block",
+                        "patternUnits": "userSpaceOnUse",
+                        "width": 50,
+                        "height": 50,
+                    });
+        large_block.append("rect")
+                        .attr({
+                            "width": 50,
+                            "height": 50,
+                            "fill": "url(#small_block)"
+                        });
+        large_block.append("path")
+                        .attr({
+                            "fill": "none",
+                            "stroke": "gray",
+                            "stroke-width": "1.5",
+                            "d": "M 50 0 L 0 0 0 50"
+                        })
+    },
+    createGroup: function(svg){
+        this.group = this.svg.append("g")
+    },
+    addAxis: function(){
+        this.group.append("path")
+            .attr({
+                "stroke": "black",
+                "stroke-width": "1",
+                "d": "M " + 0 + " " + parseFloat(this.h / 2) + " L " +  this.w+ " " + parseFloat(this.h / 2)
+            });
+        this.group.append("path")
+            .attr({
+                "stroke": "black",
+                "stroke-width": "1",
+                "d": "M " + parseFloat(this.w / 2) + " " + 0 + " L " +  parseFloat(this.w / 2)+ " " + this.h
+            })
+    },
+    createGrid: function(){
+        this.grid = this.group.append("rect")
+                            .attr({
+                                "x": 0,
+                                "y": 0,
+                                "width": this.w,
+                                "height": this.h,
+                                "stroke": "black",
+                                "stroke-width": "2",
+                                "fill": "url(#large_block)"
+                            });
+                            
+    },
+    generateGraph: function(){
+        var base = parseFloat(this.h / 2);
+        var quarter = parseFloat(this.w / 16);
+        
+        var path = [];
+        var positiveSlope = true;
+        for (var i = 0 ; i <= this.w; i += 1){
+            var rad = (i / quarter) * parseFloat(Math.PI / 2);
+            var amp = base - Math.sin(rad) * base;
+            console.log(i + ":::" + amp)
+            path.push([i, amp])
+        }
+        console.log(path);
+        this.group.append('path')
+            .attr({
+                "d": d3.svg.line().interpolate("linear")(path),
+                "stroke": "green",
+                "stroke-width": 1,
+                "fill": "none"
+            });
+    }
+    
+}
---
+++ static/js/script.js
@@ -0,0 +1,72 @@
+"use strict";
+
+function removeUnit(f){
+    return f.replace("px", "")
+}
+
+$(document).ready(function(){
+    $("#display").append($("<div></div>").addClass("viewport")
+                                        .on("mousedown", function(e){
+                                            var left = parseFloat(removeUnit($(this).css("left")));
+                                            console.log(left);
+                                            var top = parseFloat(removeUnit($(this).css("top")));
+                                            console.log(top);
+                                            console.log("Hello 2")
+                                            var offset_x = e.pageX - left;
+                                            var offset_y = e.pageY - top;
+                                            $(this).parent().data("offset", [offset_x, offset_y]);
+                                            $(this).parent().data("dragging", true);
+                                            
+                                        })
+                                        .on("mousemove", function(e){
+                                            var parent = $(this).parent();
+                                            if ((parent.data("offset") !== undefined) && (parent.data("dragging") === true)){
+                                                var offset_x = parent.data("offset")[0];
+                                                var offset_y = parent.data("offset")[1];
+                                                console.log(offset_x)
+                                                console.log(offset_y)
+                                                
+                                                var left = e.pageX -  offset_x;
+                                                var top = e.pageY -  offset_y;
+                                                
+                                                $(this).css("left", left + "px");
+                                                $(this).css("top", top + "px");
+                                            }  
+                                        })
+                                        .on("mouseup", function(){
+                                            $(this).parent().data("offset", []);
+                                            $(this).parent().data("dragging", false);
+                                        })
+                        );
+    init();
+});
+
+
+function init(){
+    $("#display div.viewport").css("left", function(){
+        return parseFloat((removeUnit($(this).parent().css("width")) - removeUnit(($(this).css("width")))) / 2) + "px";
+    });
+    $("#display div.viewport").css("top", function(){
+        return parseFloat((removeUnit($(this).parent().css("height")) - removeUnit(($(this).css("height")))) / 2) + "px";
+    });
+    var viewport = d3.select("#display div.viewport").node().getBoundingClientRect();
+    var w = viewport.width;
+    var h = viewport.height;
+
+    console.log(w);
+    console.log(h);
+    
+    var svgContainer = d3.select("#display div.viewport").append("svg")
+                            .attr("width", w)
+                            .attr("height", h)
+                            .attr("viewBox", "0 0 " + w + " " + h )
+                            .on("mousedown", function(){
+                                d3.select("#display").style("cursor", "move");
+                            })
+                            .on("mouseup", function(){
+                                d3.select("#display").style("cursor", "default"); 
+                            });
+    
+    CreateGraph.createGraph(svgContainer, w, h);
+    
+}
