a982c3ea8870f617182b633af7e759cda3829e4b
Sayantan Hore <sayantan.hore.86@gmail.com>
2015-12-29 19:49:06 +0200

    Indentation corrected contd...

--- static/js/properties.js
+++ static/js/properties.js
@@ -18,45 +18,45 @@ Graph.globals = (function(){
     var gridStrokeWidth = null;
     var axisStrokeWidth = null;
 	
-	(function __init(){
-		smallBlockDim = 10;
-		largeBlockDim = 100;
+    (function __init(){
+        smallBlockDim = 10;
+        largeBlockDim = 100;
         
         smallBlockStrokeWidth = 0.2;
         largeBlockStrokeWidth = 0.4;
         gridStrokeWidth = 2;
         axisStrokeWidth = 0.5;
-	})();
-	
-	var callables = {
-		setScreenDim: function($window){
-			screenWidth = $window.width();
-			screenHeight = $window.height();
-		},
+    })();
+
+    var callables = {
+        setScreenDim: function($window){
+            screenWidth = $window.width();
+            screenHeight = $window.height();
+        },
         
-		getScreenDim: function(){
-			return {
-				screenWidth: screenWidth,
-				screenHeight: screenHeight
-			}
-		},
+        getScreenDim: function(){
+            return {
+                screenWidth: screenWidth,
+                screenHeight: screenHeight
+            }
+        },
         
-		setBlockDim: function(blockDim){
-			if (blockDim.hasOwnProperty('smallBlockDim') && blockDim.smallBlockDim !== null){
-				smallBlockDim = blockDim.smallBlockDim;
-			}
-			if (blockDim.hasOwnProperty('largeBlockDim') && blockDim.largeBlockDim !== null){
-				largeBlockDim = blockDim.largeBlockDim;
-			}
-		},
-		
-		getBlockDim: function(){
-			return {
-				smallBlockDim: smallBlockDim,
-				largeBlockDim: largeBlockDim
-			}
-		},
-		
+        setBlockDim: function(blockDim){
+            if (blockDim.hasOwnProperty('smallBlockDim') && blockDim.smallBlockDim !== null){
+                smallBlockDim = blockDim.smallBlockDim;
+            }
+            if (blockDim.hasOwnProperty('largeBlockDim') && blockDim.largeBlockDim !== null){
+                largeBlockDim = blockDim.largeBlockDim;
+            }
+        },
+        
+        getBlockDim: function(){
+            return {
+                smallBlockDim: smallBlockDim,
+                largeBlockDim: largeBlockDim
+            }
+        },
+        
         getStrokeWidth: function(){
             return {
                 smallBlock: smallBlockStrokeWidth,
@@ -70,25 +70,25 @@ Graph.globals = (function(){
             return origin;
         },
         
-		createPathString: function(dim){
-			return 'M ' + dim + ' 0 L 0 0 0 ' + dim
-		},
-		
-		getPathAxisX: function(){
-			var startY = Math.ceil((parseFloat(screenHeight) / 2) / largeBlockDim);
+        createPathString: function(dim){
+            return 'M ' + dim + ' 0 L 0 0 0 ' + dim
+        },
+        
+        getPathAxisX: function(){
+            var startY = Math.ceil((parseFloat(screenHeight) / 2) / largeBlockDim);
             origin.y = startY * largeBlockDim;
-			console.log(origin.y);
-			return 'M 0 ' + origin.y + ' H ' + screenWidth;
-		},
-		
-		getPathAxisY: function(){
-			var startX = parseInt((parseFloat(screenWidth) / 2) / largeBlockDim);
+            console.log(origin.y);
+            return 'M 0 ' + origin.y + ' H ' + screenWidth;
+        },
+        
+        getPathAxisY: function(){
+            var startX = parseInt((parseFloat(screenWidth) / 2) / largeBlockDim);
             origin.x = startX * largeBlockDim;
-			console.log(origin.x);
-			return 'M ' + origin.x + ' 0 V ' + screenHeight;
-		}
-		
-	}
-	
-	return callables;	
+            console.log(origin.x);
+            return 'M ' + origin.x + ' 0 V ' + screenHeight;
+        }
+        
+    }
+
+    return callables;	
 })();
