16839742dab697de7c3304eda1e29d525c5d2ca0
Sayantan Hore <sayantan.hore.86@gmail.com>
2016-02-21 00:11:24 +0200

    'properties.js' simplified

--- static/js/properties.js
+++ static/js/properties.js
@@ -3,6 +3,41 @@
 var Graph = Graph || {};
 
 Graph.globals = (function(){
+    var smallBlockDim = 10;
+    var largeBlockDim = 100;
+    var smallBlockStrokeWidth = 0.2;
+    var largeBlockStrokeWidth = 0.4;
+    var gridStrokeWidth = 2;
+    var axisStrokeWidth = 0.5;
+    var origin = {
+        x: 0,
+        y: 0
+    };
+    
+    return {
+        getBlockDim: function(){
+            return {
+                smallBlockDim: smallBlockDim,
+                largeBlockDim: largeBlockDim
+            }
+        },
+        
+        getStrokeWidth: function(){
+            return {
+                smallBlock: smallBlockStrokeWidth,
+                largeBlock: largeBlockStrokeWidth,
+                grid: gridStrokeWidth,
+                axis: axisStrokeWidth
+            }
+        },
+        createPathString: function(dim){
+            return 'M ' + dim + ' 0 L 0 0 0 ' + dim
+        }
+    };
+})();
+
+/*
+Graph.globals = (function(){
     var screenWidth = null;
     var screenHeight = null;
     var smallBlockDim = null;
@@ -92,3 +127,5 @@ Graph.globals = (function(){
 
     return callables;	
 })();
+
+*/
