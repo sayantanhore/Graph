9ba7f5acd18219ddc3d41e3dd795ba9f9d2aebd2
Sayantan Hore <sayantan.hore.86@gmail.com>
2016-08-15 00:22:03 +0300

    Browserify integrated in karma

--- karma.conf.js
+++ karma.conf.js
@@ -10,13 +10,11 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['browserify', 'jasmine'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      'bower_components/d3/d3.js',
-      'bower_components/jquery/dist/jquery.js',
       'src/**/*spec.js'
     ],
 
@@ -29,6 +27,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/**/*spec.js': ['browserify']
     },
 
 
