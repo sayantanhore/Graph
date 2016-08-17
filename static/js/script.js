22dc574c153b37ba99b3ce0cfea78e5611227f24
Sayantan Hore <sayantan.hore@vertoanalytics.com>
2016-08-17 16:51:43 +0300

    Cleaned up: static/ and unit-test-client/ removed

--- static/js/script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-$(document).ready(function(){
-    console.log("Ready");
-    console.log("----------------------------");
-    __init("#container");
-});
--- static/js/ui/ui.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function __init(container){
-    if (!Graph.ui.Grid){
-        console.log("Grid did not load properly");
-    }
-    var grid = Object.create(Graph.ui.Grid);
-    grid.init(container);
-};
